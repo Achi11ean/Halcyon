d6f7b02aed9c3a4b0c554a5a7228912587c54d78
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-07-29 13:57:15 -0400

    Halcyon

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #Halcyon
+The purpose of this entity is to spread peaceful and insightful poetic creations with the world.
--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "our troubles are our victory";
-document.querySelector("body").appendChild(h2);
+const email = document.getElementById('email');
+console.log('email is: ', email);
+email.addEventListener('mouseover', function() {
+    alert('Please send all email inquiries to: paredes.Jonathen@yahoo.com');
+});
+
+const input = document.getElementById('phone');
+phone.addEventListener('mouseover', function() {
+    alert('Leave a voicemail inquiry to: 959.204.1689');
+});
