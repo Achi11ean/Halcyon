aed9ad6207737b083fa6f56308e4aef03be380ea
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-10-30 15:01:07 -0400

    completed for now

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const input = document.getElementById('phone');
 phone.addEventListener('mouseover', function() {
     alert('Leave a voicemail inquiry to: 959.204.1689');
 });
+document.getElementById("toggleButton").addEventListener("click", function () {
+    const content = document.getElementById("content");
+    const button = document.getElementById("toggleButton");
+    
+    if (content.style.display === "none") {
+        content.style.display = "block";
+        button.textContent = "I Got it, Thanks!";
+    } else {
+        content.style.display = "none";
+        button.textContent = "Tell Me More!";
+    }
+});
+
